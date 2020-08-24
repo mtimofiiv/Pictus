23a291b107fa6c43a2316f51c9175301d794a271
joe <joeczepil@gmail.com>
2020-08-24 14:55:59 -0700

    Add share image

--- src/templates/seo.js
+++ src/templates/seo.js
@@ -1,7 +1,7 @@
 import React from "react"
 import Helmet from "react-helmet"
 import favicon from '../images/path.png'
-import share from '../images/path.png'
+import share from '../images/share.png'
 
 const SEO = ({ description, lang, meta, title, canonical, keywords }) => {
 
@@ -26,12 +26,12 @@ const SEO = ({ description, lang, meta, title, canonical, keywords }) => {
       <meta property="og:title" content="PICTUS - Making it easier than ever for your recycling stewardship" />
       <meta property="og:type" content="website" />
       <meta property="og:url" content="https://www.pictus.ca/" />
-      {share && <meta property="og:image" content={share} />}
+      <meta property="og:image" content={share} />
       <meta property="og:description" content="PICTUS empowers recycling stewardships and PROs to manage, monitor, and improve their recycling and incentive programs. Configure your application today and see the results tomorrow." />
 
       <meta name="twitter:title" content="PICTUS - Making it easier than ever for your recycling stewardship"/>
       <meta name="twitter:description" content="PICTUS empowers recycling stewardships and PROs to manage, monitor, and improve their recycling and incentive programs. Configure your application today and see the results tomorrow."/>
-      {share && <meta name="twitter:image" content={share} />}
+      <meta name="twitter:image" content={share} />
       <meta name="twitter:card" content="summary_large_image"/>
       <meta property="twitter:url" content="https://www.pictus.ca"></meta>
 
